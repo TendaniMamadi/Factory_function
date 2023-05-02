8794112ccc1d0e5b735d99d6063d99008b157fcf
TendaniMamadi <mamadi.tendani@gmail.com>
2023-05-02 16:09:54 +0200

    calculateBill

--- test/calculate bill.test.js
+++ test/calculate bill.test.js
@@ -1,34 +1,34 @@
-describe("The calculate bill function",function(){
-    it("should take a call string and return its value",function(){
+describe("The calculate bill function", function () {
+    it("should take a call string and return its value", function () {
         let calculate_bill = calculateBill()
         calculate_bill.setCallCost("call")
-       
+
         assert.equal(2.75, calculate_bill.getCallCost());
     })
 
-    it("should take an sms string and return its value",function(){
-       let calculate_bill2 = calculateBill()
-       calculate_bill2.setSmsCost("sms")
-       
+    it("should take an sms string and return its value", function () {
+        let calculate_bill2 = calculateBill()
+        calculate_bill2.setSmsCost("sms")
+
         assert.equal(0.75, calculate_bill2.getSmsCost())
 
     })
 
-   
-    it("should return 0 if passed with empty string",function(){
+
+    it("should return 0 if passed with empty string", function () {
         let calculate_bill4 = calculateBill()
         calculate_bill4.getEmptyString("")
-        
-         assert.equal("", calculate_bill4.getEmptyString())
- 
-     })
 
-     it("should return total of call and sms",function(){
+        assert.equal("", calculate_bill4.getEmptyString())
+
+    })
+
+    it("should return total of call and sms", function () {
         let text_bill3 = calculateBill()
-        
+
         text_bill3.getTotalCost()
-        
-         assert.equal(0.00, text_bill3.getTotalCost())
- 
-     })
+
+        assert.equal(0.00, text_bill3.getTotalCost())
+
+    })
 })
